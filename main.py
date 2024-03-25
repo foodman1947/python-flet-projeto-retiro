d868eb47abfe9017c1e2404c245bb388e19304ae
joão paulo salvador de moura <joaomouratec@gmail.com>
2024-03-24 21:19:44 -0300

    adicionado sistema de login e algumas estilizações para pagina de login, sistema de login ainda falho porém a logica está sendo construida

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import flet as ft
 from pages.login import LoginPage as lp
 
 def main(page: ft.Page):
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     login = lp()
     page.add(login)
-
+    
 ft.app(main)
--- pages/login.py
+++ pages/login.py
@@ -1,15 +1,31 @@
 import flet as ft 
 
-class LoginPage(ft.UserControls):
+class LoginPage(ft.UserControl):
     def build(self):
+        self.usuario=ft.TextField(label="login")
+        self.senha=ft.TextField(label="senha", password=True, can_reveal_password=True)     
+
         return ft.Container(
-            width = 600,
-            bgcolor = ft.colors.BLUe,
+            width = 300,
+            bgcolor = ft.colors.BLUE,
+            padding = 30,
+            border_radius=6,
             content = ft.Column(
                 [
-                    ft.TextField(label="login"),
-                    ft.TextField(label="senha", password=True, can_reveal_password=True),
-                    ft.ElevatedButton('Entrar')
+                    self.usuario,
+                    self.senha,
+                    ft.Row([ft.ElevatedButton('Entrar', on_click=self.verificarLogin)], vertical_alignment=ft.MainAxisAlignment.END)
                 ]
             ) 
         )
+    def verificarLogin(self, e):
+            if self.usuario =="":
+                self.usuario.error_text="Campo de usuario vazio"
+            elif self.senha =="":
+                self.senha.error_text="Campo de senha vazia"
+            elif self.usuario =='' and self.senha =="":
+                self.senha.error_text="preencha o login e a senha!!"
+            elif self.usuario == "fernando" and self.senha =="fernando1234@":
+                print('bora para outra pagina mermão')
+            elif self.usuario == "joão" and self.senha == "91710972":
+                print('bora logar joão')
